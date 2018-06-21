75744af9740eb53b9d947ed98fdf4401bead4ee9
Saravanan Kandaswamy <ksnblr@gmail.com>
2018-06-21 16:04:11 -0400

    commited for route.py changes

--- routes.py
+++ routes.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
 @app.route("/about")
-def index():
+def about():
     return render_template("about.html")
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
