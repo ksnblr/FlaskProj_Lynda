d195e2c8f3667446cdbb326f7d95e3663253de00
Saravanan Kandaswamy <ksnblr@gmail.com>
2018-06-22 10:27:13 -0400

    routes.py updated.

--- routes.py
+++ routes.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, request
+from models import db, User
 from forms import SignupForm
+
 app = Flask(__name__)
 
-app.config ['SQLALCHEMY_DATABSE_URI'] = 'postgressql://localhost/learningflask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
 db.init_app(app)
+
 app.secret_key = "development-key"
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+  return render_template("index.html")
+
 @app.route("/about")
 def about():
-    return render_template("about.html")
-@app.route("/signup")
+  return render_template("about.html")
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    form = SignupForm()
-    return render_template("signup.html", form=form)
+  form = SignupForm()
+  if request.method == "POST":
+      return 'Success!'
+  elif request.method == "GET":
+      return render_template('signup.html', form=form)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
